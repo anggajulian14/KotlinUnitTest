9f05feb0624d2ab238ebf94d8b7ff01172ebb004
Angga Julian <anggajulian035@gmail.com>
2023-11-04 10:36:56 +0700

    menambahkan class SimpleCalculator

---
+++ app/src/main/java/com/example/kotlinunittest/SimpleCalculator.kt
@@ -0,0 +1,10 @@
+package com.example.kotlinunittest
+
+class SimpleCalculator {
+    // Fungsi add menerima dua parameter bertipe data Int, yaitu a dan b.
+    // Fungsi ini menjumlahkan nilai a dan b, lalu mengembalikan hasilnya sebagai Int.
+    fun add(a: Int, b: Int): Int {
+        // Mengembalikan hasil penjumlahan dari parameter a dan b.
+        return a + b
+    }
+}
